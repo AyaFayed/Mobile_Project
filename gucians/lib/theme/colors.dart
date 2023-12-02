d321682c7958171f4f59b5d0220c3ccbfe4ca255
Aya Ahmed <79098945+AyaFayed@users.noreply.github.com>
2023-12-02 15:31:02 +0200

    initial project structure

---
+++ gucians/lib/theme/colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primary = const Color.fromARGB(255, 191, 26, 47);
+  static Color secondary = const Color.fromARGB(255, 240, 173, 41);
+  static Color dark = const Color.fromARGB(255, 50, 55, 59);
+  static Color light = Colors.white;
+  static Color success = const Color.fromARGB(255, 50, 55, 59);
+  static Color error = const Color.fromARGB(255, 191, 26, 47);
+  static Color confirm = Colors.black;
+  static Color selected = Colors.black;
+  static Color unselected = Colors.grey;
+  static Color lightGrey = Colors.grey[200]!;
+}
---
+++ gucians/lib/theme/themes.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:gucians/theme/colors.dart';
+
+class CustomTheme {
+  static ThemeData get lightTheme {
+    return ThemeData(
+        fontFamily: 'Poppins',
+        colorScheme: ColorScheme(
+            brightness: Brightness.light,
+            primary: AppColors.primary,
+            onPrimary: Colors.white,
+            secondary: AppColors.secondary,
+            onSecondary: Colors.white,
+            error: AppColors.error,
+            onError: Colors.white,
+            background: Colors.white,
+            onBackground: Colors.black,
+            surface: Colors.white,
+            onSurface: Colors.black));
+  }
+}
